20710529f24ea1caaf19fa1f45302471a0b7e998
Vaikundhavasan <121146276+Vaikundhavasan@users.noreply.github.com>
2023-10-20 11:26:50 +0530

    Update Employee.js

--- Server/models/Employee.js
+++ Server/models/Employee.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
+  regNo: String,
   name: String,
-  email: String,
-  password: String,
+  mobileNo: String,
+  dob: String,
+  batch: String,
+  dept: String,
+  parent: String,
+  parentNo: String,
+  address: String,
+  zip: String,
+  city: String,
+  dist: String,
+  state: String,
 });
 
 const EmployeeModel = mongoose.model("employees", EmployeeSchema);
